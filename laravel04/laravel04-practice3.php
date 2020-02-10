d162104f5b1280b7b8fb3f4f6e56ef14860643ac
三好 <m.sanhao@gmail.com>
2020-02-10 07:52:29 +0000

    first commit

---
+++ laravel04/laravel04-practice3.php
@@ -0,0 +1,11 @@
+<?php
+
+function array_multiplication($arr) {
+    $count = count($arr);
+    $result = 0;
+    for($i = 0; $i <= $count-1){
+    $result = array[0] * array 
+    }
+    return $result;
+}
+echo array_multiplication(array(1,3,5,7,9))
