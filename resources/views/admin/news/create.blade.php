35f16b80e1dd3f51cffda19183f308410cb17223
三好和之 <miyoshikazuyuki@miyoshikazuyukinoMacBook-puro.local>
2020-03-04 18:41:49 +0900

    layout, laravel Mix, CSSコンパイルまで

--- resources/views/admin/news/create.blade.php
+++ resources/views/admin/news/create.blade.php
@@ -6,6 +6,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
         <title>MyNews</title>
+
+        @extends('layouts.admin')
+        @section('title', 'ニュースの新規作成')
+        @section('content')
+            <div class="container">
+                <div class="rouw">
+                    <div class="col-md-8 mx-auto">
+                        <h2>ニュース新規作成</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
+        
     </head>
     <body>
         <h1>Myニュース作成画面</h1>
