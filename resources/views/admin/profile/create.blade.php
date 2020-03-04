437c4f52ea4743d6a2c1d197782ad3f0e77a1f3c
三好和之 <miyoshikazuyuki@miyoshikazuyukinoMacBook-puro.local>
2020-03-04 18:54:10 +0900

    Laravel11課題3~7まで

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -6,6 +6,19 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Profile</title>
+
+        @extends('layouts.profile')
+        @section('title', 'プロフィールの新規作成')
+        @section('content')
+            <div class="container">
+                <div class="rouw">
+                    <div class="col-md-8 mx-auto">
+                        <h2>プロフィール新規作成</h2>
+                    </div>
+                </div>
+            </div>
+        @endsection
+
     </head>
     <body>
         <h1>プロフィール作成画面</h1>
